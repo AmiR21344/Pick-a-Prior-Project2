b6d2d06e0b3b9e57ebb2df322952f9222b316598
AmirASC21 <87777101+AmiR21344@users.noreply.github.com>
2021-08-09 21:16:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let wow = document.querySelector("div.column");
+wow.onmouseover = function () {
+  wow.style.cursor = "pointer";
+};
+let wow1 = document.querySelector("div.column2");
+wow1.onmouseover = function () {
+  wow1.style.cursor = "pointer";
+};
+let wow2 = document.querySelector("div.column3");
+wow2.onmouseover = function () {
+  wow2.style.cursor = "pointer";
+};
+let wow3 = document.querySelector("div.column4");
+wow3.onmouseover = function () {
+  wow3.style.cursor = "pointer";
+};
+let wow4 = document.querySelector("div.column5");
+wow4.onmouseover = function () {
+  wow4.style.cursor = "pointer";
+};
+let wow5 = document.querySelector("div.column");
+wow5.ondragcenter = function () {
+  wow5.style.cursor = "grabbing";
+};
+// window.onscroll = function(){scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("items").style.padding = "0px";
+//     document.getElementsByClassName("word").style.fontSize = "15px";
+//   } else {
+//     document.getElementById("items").style.padding = "0";
+//     document.getElementsByClassName("word").style.fontSize = "35px";
+//   }
+// }
